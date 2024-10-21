55d04ab1da69885b6c5bd38ff4255aeae433a633
antoniofmoliveira <antoniofmoliveia@outlook.com>
2024-10-21 09:20:44 -0300

    feat: initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/antoniofmoliveira/fullcycle-desafio-stresstest
+
+go 1.23.2
+
+require golang.org/x/text v0.19.0
---
+++ main.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"sort"
+	"time"
+
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
+)
+
+type Red struct {
+	Target     string
+	SentAt     time.Time
+	ReceivedAt time.Time
+	StatusCode int
+}
+
+var client *http.Client
+
+// init sets up the http client to use a Transport with a large number of
+// idle connections and a long idle connection timeout. This is so that the
+// client can handle a large number of concurrent requests without having to
+// do a lot of extraneous work.
+func init() {
+	tr := &http.Transport{
+		MaxIdleConns:        100,
+		IdleConnTimeout:     30 * time.Second,
+		MaxIdleConnsPerHost: 100,
+	}
+	client = &http.Client{Transport: tr}
+}
+
+// Get performs a GET request to the URL in r.Target, retrying up to three
+// times if there is an error. It records the time at which the request was
+// sent and the time at which the response was received. It also records the
+// status code of the response. If any of the requests fail due to an error,
+// the final status code is -1.
+func (r *Red) Get() *Red {
+	req, err := http.NewRequest("GET", r.Target, nil)
+	if err != nil {
+		panic(err)
+	}
+	r.SentAt = time.Now()
+	var res *http.Response
+	for i := 0; i < 3; i++ {
+		time.Sleep(1 * time.Microsecond)
+		res, err = client.Do(req)
+		if err == nil {
+			break
+		}
+	}
+	if err != nil {
+		log.Println(err)
+		r.ReceivedAt = time.Now()
+		r.StatusCode = -1
+		return r
+	}
+
+	_, err = io.Copy(io.Discard, res.Body)
+	if err != nil {
+		log.Println(err)
+	}
+	defer res.Body.Close()
+	r.ReceivedAt = time.Now()
+	r.StatusCode = res.StatusCode
+	return r
+}
+
+// RedRoutine runs a single request to the target URL and sends the result
+// down the channel. It retries up to three times if there is an error.
+func RedRoutine(target string, rec chan *Red) {
+	r := &Red{
+		Target: target,
+	}
+	rec <- r.Get()
+}
+
+type Result struct {
+	NumRequestPerSecond          int
+	NumRequestWithErrorPerSecond int
+	NumNetworkErrorPerSecond     int
+	AverageDuration              time.Duration
+}
+
+// calculateRed takes a slice of Red structs and returns a map where the keys
+// are strings representing the minute and second the request was sent, and the
+// values are Results which contain the total number of requests sent in that
+// second, the number of requests that returned an error, and the average
+// duration of the requests in that second.
+func calculateRed(recs []*Red) map[string]*Result {
+
+	mapRecs := make(map[string]*Result)
+
+	for _, rec := range recs {
+		s := fmt.Sprintf("%d", rec.SentAt.Minute()*100+rec.SentAt.Second())
+		if mapRecs[s] == nil {
+			mapRecs[s] = &Result{}
+		}
+		mapRecs[s].NumRequestPerSecond++
+		if rec.StatusCode == -1 {
+			mapRecs[s].NumNetworkErrorPerSecond++
+		}
+		if rec.StatusCode != 200 && rec.StatusCode != -1 {
+			mapRecs[s].NumRequestWithErrorPerSecond++
+		}
+		mapRecs[s].AverageDuration = mapRecs[s].AverageDuration + rec.ReceivedAt.Sub(rec.SentAt)
+	}
+	return mapRecs
+}
+
+// main is the entry point for the CLI application. It parses command-line flags
+// to determine the number of tests, the endpoint to test, and the interval
+// between requests. It initiates a series of requests to the specified endpoint
+// using goroutines, collects the results, and processes them to calculate RED
+// metrics. Finally, it prints a summary of these metrics, showing the number of
+// requests per second, errors, average request duration, and network errors.
+func main() {
+
+	n := flag.Int("numtests", 300000, "Number of tests")
+	endpoint := flag.String("endpoint", "http://localhost:8080/hello", "Endpoint")
+	interval := flag.Int("interval", 1, "Interval in microseconds")
+	flag.Parse()
+
+	numtests := *n
+	inter := *interval
+	recs := []*Red{}
+
+	log.Println("Tests starting...")
+	fmt.Println(" Running ", numtests, " tests with interval ", inter, " microseconds and endpoint ", *endpoint)
+
+	rec := make(chan *Red)
+	for i := 0; i < numtests; i++ {
+		if inter > 0 {
+			time.Sleep(time.Duration(inter) * time.Microsecond)
+		}
+		go RedRoutine(*endpoint, rec)
+	}
+
+	for i := 0; i < numtests; i++ {
+		r := <-rec
+		recs = append(recs, r)
+	}
+
+	result := calculateRed(recs)
+
+	keys := make([]string, 0, len(result))
+	for k := range result {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	p := message.NewPrinter(language.English)
+	fmt.Printf("%-7s\t%10s\t%10s\t%10s\t%10s\n", "Min/Seg", "Rate", "Error", "Avg Time", "Net Error")
+	for _, v := range keys {
+		fmt.Printf("%-7s\t%10s\t%10s\t%10v\t%10s\n", v, p.Sprintf("%d", result[v].NumRequestPerSecond), p.Sprintf("%d", result[v].NumRequestWithErrorPerSecond), result[v].AverageDuration/time.Duration(result[v].NumRequestPerSecond-result[v].NumNetworkErrorPerSecond+1), p.Sprintf("%d", result[v].NumNetworkErrorPerSecond))
+	}
+	log.Println("Tests finished.")
+
+}
